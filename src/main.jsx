b5cf7a02ad4d16de0f633aebbeff1b5773233a64
waltershaquil <81990995+waltershaquil@users.noreply.github.com>
2025-01-12 00:32:01 +0200

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import TaskPage from "./pages/taskpage";
+import TaskPage from "./pages/Taskpage";
 
 const router = createBrowserRouter([
   {
