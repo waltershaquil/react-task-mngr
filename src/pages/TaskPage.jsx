c3f10f3527e4f0e25af0da6f99d576ae9b481e00
waltershaquil <81990995+waltershaquil@users.noreply.github.com>
2025-01-12 00:35:36 +0200

    more

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -9,7 +9,7 @@ function TaskPage() {
     <div className="w-screen h-screen bg-gray-800 text-center flex flex-col justify-center items-top p-6">
       <div class="flex justify-center items-center min-h-screen">
         <div class="max-w-[720px] mx-auto">
-          <div class="relative flex flex-col mt-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96 hover:border-slate-300 hover:shadow-md rounded-lg transition-all cursor-pointer">
+          <div class="relative flex flex-col mt-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96 hover:border-slate-300 hover:shadow-md transition-all cursor-pointer">
             <div class="p-4">
               <h5 class="mb-2 text-slate-800 text-xl font-semibold">{name}</h5>
               <p class="text-slate-600 leading-normal ">{description}</p>
